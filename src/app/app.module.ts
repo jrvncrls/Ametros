d1beddf2e3d9525346397191da90294dacae7c72
jervincarlos <jervin@sliqpay.com>
2022-11-06 17:30:36 +0800

    Add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,10 +14,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { HeaderComponent } from './shared/header/header.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, HeaderComponent, HomeComponent],
+  declarations: [AppComponent, LoginComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -31,6 +31,7 @@ import { HeaderComponent } from './shared/header/header.component';
     NgbModule,
     FormsModule,
     ReactiveFormsModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
